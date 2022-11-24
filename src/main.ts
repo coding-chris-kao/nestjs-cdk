cd875b8007fe7437f8c5765ae4ba3e8407361a9e
coding-chris-kao <chi1010860@gmail.com>
2022-11-24 11:18:21 +0800

    fix: add useMiddleware in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { useMiddleware } from './middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  useMiddleware(app);
   await app.listen(3000);
 }
 bootstrap();
