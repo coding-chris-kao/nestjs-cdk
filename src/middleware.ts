cd5f655de74e27a12213cc8d2792495680e6454b
coding-chris-kao <chi1010860@gmail.com>
2022-11-24 11:18:46 +0800

    feat: improve document

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,32 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { json } from 'body-parser';
 
 export function useMiddleware(app: INestApplication) {
   app.enableCors();
   app.use(json());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   useSwagger(app);
 }
 
 function useSwagger(app: INestApplication) {
-  const config = new DocumentBuilder()
+  const urlPrefix = '';
+  let jsonLink = '/document-json';
+
+  const configBuilder = new DocumentBuilder()
     .setTitle('Serverless-NestJS')
-    .setDescription('Basic Swagger document.')
-    .setVersion('1.0')
-    .build();
+    .setVersion('1.0');
+
+  if (process.env.NODE_ENV !== 'development') {
+    configBuilder.addServer(urlPrefix);
+    jsonLink = urlPrefix + jsonLink;
+  }
+
+  configBuilder.setDescription(
+    `Right Click to get <a href="${jsonLink}">JSON Link<a/>`,
+  );
+
+  const config = configBuilder.build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('document', app, document);
 }
